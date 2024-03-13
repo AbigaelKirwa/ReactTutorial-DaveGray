fc6670336df2a9d2ec03b6b1324dc07b8b2caef4
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-03-13 19:30:55 +0200

    feat(handling lists):creates a list that is stored in localstorage and has check and delete functionality implemented using event handlers

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,62 @@
 import {React, useState} from 'react'
+import {FaTrashAlt} from "react-icons/fa"
 
 const Content = () => {
-    const [name, setName] = useState("Aby")
-    const [count, setCount] = useState(0)
-
-    const handleNameChange = () => {
-        const names = ["Aby", "Jerald", "Mokaya"];
-        const int = Math.floor(Math.random()*3)
-        setName(names[int])
+    const [items, setItems] = useState([
+        {
+            id:1,
+            checked:true,
+            item:"one half pound of cocoa covered almonds unstalted"
+        },
+        {
+            id:2,
+            checked:false,
+            item:"Item 2"
+        },
+        {
+            id:3,
+            checked:false,
+            item:"Item 3"
+        },
+    ]);
+    const handleCheck = (id) =>{
+        const listItems = items.map((item)=> 
+            item.id === id ? {...item, checked:!item.checked} : item
+        )
+        setItems(listItems)
+        localStorage.setItem('shopping list', JSON.stringify(listItems))
     };
-
-    const handleClick = () =>{
-        setCount(count + 1)
-        console.log(count)
-    };
-
-    const handleClick2 = (name) =>{
-        console.log(`${name} was clicked`)
-    };
-
-    const handleClick3 = (e) =>{
-        console.log(e.target.innerText)
-    };
-
+    const handleDelete = (id) =>{
+        const listItems = items.filter((item)=>item.id != id)
+        setItems(listItems)
+        localStorage.setItem('shopping list', JSON.stringify(listItems))
+    }
     return (
         <main>
-            <p>Hello {name}</p>
-            <button onClick={handleNameChange}>Change Name</button>
-            <button onClick={handleClick}>Add count</button>
-            <button onClick={(e)=>handleClick3(e)}>Click me</button>
+            {items.length ? (
+            <ul>
+                {items.map((item)=>(
+                    <li className='item' key={item.id}>
+                        <input
+                            type='checkbox'
+                            onChange={()=>handleCheck(item.id)}
+                            checked={item.checked}
+                        />
+                        <label
+                            style={(item.checked ? {textDecoration:"line-through"} : null)}
+                            onDoubleClick={()=>{handleCheck(item.id)}}
+                        >{item.item}</label>
+                        <FaTrashAlt 
+                            onClick={()=>{handleDelete(item.id)}}
+                            role='button' 
+                            tabIndex="0"
+                        />
+                    </li>
+                ))}
+            </ul>
+            ): 
+            <p>List is empty</p>
+            }
         </main>
     )
 }
